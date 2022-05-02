3efe06bc31c192d24ece346ad963a2aa318a1b0a
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-03 00:58:05 +0300

    ft:add news top headlines column

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask,render_template
+from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def home():
+    newsapi = NewsApiClient(api_key="8fef44d4c9bd4e70b5c1360222846157")
     
+    top_headlines = newsapi.get_top_headlines(sources='cnn')
+    
+    t_article = top_headlines['articles']
+    
+    news =[]
+    desc =[]
+    img =[]
+    p_date =[]
+    url =[]
     return render_template('home.html')
 
 if __name__ == '__main__':
